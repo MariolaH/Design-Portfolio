1977c60ef3a799e7caace0f19c0732abf2eb36e4
Mariola Hullings <mariolahullings@gmail.com>
2024-06-05 22:59:20 -0400

    inital commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
     <div>
       {/* <header className="text-center text-light py-5" style={{ backgroundImage: 'url("/crystal-7428278_1280.jpg")', backgroundSize: 'cover', backgroundPosition: 'center' }}>
         <h1 className="display-4">Hi, I'm Mariola</h1> */}
-        {/* <p className="lead">A FREE RESPONSIVE TEMPLATE BY WEBFLOW</p> */}
-        {/* <div className="mt-4">
+      {/* <p className="lead">A FREE RESPONSIVE TEMPLATE BY WEBFLOW</p> */}
+      {/* <div className="mt-4">
           <Link href="/signup" legacyBehavior>
             <a className="btn btn-primary mx-2">Sign Up</a>
           </Link>
@@ -31,7 +31,6 @@ export default function Home() {
           </Link>
         </div>
       </header>
-  
       <section className="container text-center py-5">
         <h2 className="mb-4">UX / UI DESIGN</h2>
         <p className="text-muted mb-5">THIS IS SOME TEXT INSIDE OF A DIV BLOCK.</p>
@@ -44,6 +43,7 @@ export default function Home() {
                 <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
               </a>
             </Link>
+
           </div>
           <div className="col-md-4 mb-4">
             <Link href="/case-studies/awesome-code" legacyBehavior>
