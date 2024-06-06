aefd61d5246de68b19569d320bb446e43d77e1ec
Mariola Hullings <mariolahullings@gmail.com>
2024-06-06 15:42:26 -0400

    updated Link

---
+++ src/app/bio/pages.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Link from "next/link";
+import '../app/styles/global.css'
+import Image from "next/image";
+
+export default function bio() {
+    return (
+      <div>
+        <h1>About Me</h1>
+        <p>Hi, I'm Mariola, a UX/UI Designer and Software Developer.</p>
+        {/* Add more content about yourself here */}
+      </div>
+    );
+  };
+  
+  
