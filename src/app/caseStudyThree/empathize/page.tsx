538a6e566a68855ab7b8d33f49761f5cfc1fbbb2
Mariola Hullings <mariolahullings@gmail.com>
2024-06-10 14:00:45 -0400

    Added case studies files

---
+++ src/app/caseStudyThree/empathize/page.tsx
@@ -0,0 +1,150 @@
+"use client";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import div from "next/link";
+import "/src/app/styles/global.css";
+
+export default function empathize() {
+  return (
+    <div>
+      <section className="container text-center py-5">
+        <h2 className="mb-5 mt-5">
+          Introduction to the Empathize Section Phase
+        </h2>
+
+        <div className="row justify-content-center mt-5">
+          <div className="col-md-2 d-flex justify-content-center">
+            <div className="p-4 d-block text-decoration-none">
+              <div className="content">
+                <p>
+                  Welcome to the Empathize section of my portfolio. This section
+                  is dedicated to the foundational phase of the design thinking
+                  process—empathizing with users. Here, you will find an
+                  exploration of various methods and techniques used to deeply
+                  understand user needs, pain points, and goals.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row justify-content-center">
+          <div className="col-md-2 d-flex justify-content-center">
+            <div className="p-4 d-block text-decoration-none">
+              <div className="content">
+                <h5 className="mb-4">
+                  The Importance of Empathizing with Users
+                </h5>
+                <p>
+                  Empathy is the cornerstone of effective UX design. By
+                  genuinely understanding and connecting with users, designers
+                  can create solutions that are not only functional but also
+                  meaningful and engaging. This deep understanding ensures that
+                  the products we create truly resonate with users, leading to
+                  more impactful and lasting experiences.
+                </p>
+                <p>
+                  Empathizing with users is not just the first step in the
+                  design process—it is a continuous effort that informs every
+                  subsequent phase. By thoroughly understanding the users, we
+                  can ensure that the design solutions are tailored to their
+                  real needs and challenges. This results in products that are
+                  more likely to be successful and well-received.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row justify-content-center">
+          <div className="col-md-2 d-flex justify-content-center">
+            <div className="p-4 d-block text-decoration-none">
+              <div className="content">
+                <h5 className="mb-4">What You Will See in This Section</h5>
+                <p>
+                  In this section, you will be introduced to several key empathy
+                  activities that are crucial for gathering insights and
+                  creating user-centered designs:
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row justify-content-center">
+          <div className="col-md-2 d-flex justify-content-center">
+            <div className="around p-4 d-block shadow text-decoration-none">
+              <div className="info">
+                <img src="/empathizeCS3/researchGoals.png" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row justify-content-center mt-5">
+          <div className="col-md-2 d-flex justify-content-center">
+            <div className="around p-4 d-block shadow text-decoration-none">
+              <div className="info">
+                <img src="/empathizeCS3/audience.png" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row justify-content-center mt-5">
+          <div className="col-md-2 d-flex justify-content-center">
+            <div className="around p-4 d-block shadow text-decoration-none">
+              <div className="info">
+                <img src="/empathizeCS3/interviewQuestions.png" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-md-2 d-flex justify-content-center">
+            <div className="p-4 d-block text-decoration-none">
+              <div className="size2">
+                <p className="mt-4">
+                User Interviews: Detailed documentation and analysis of interviews conducted to gather firsthand user insights.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row justify-content-center mt-3">
+          <div className="col-md-2 d-flex justify-content-center">
+            <div className="around p-4 shadow text-decoration-none">
+              <div className="info">
+                <img src="/empathizeCS3/Anna.png" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row justify-content-center mt-5">
+          <div className="col-md-2 d-flex justify-content-center">
+            <div className="around p-4 d-block shadow text-decoration-none">
+              <div className="img-fluid"></div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row justify-content-center">
+          <div className="col-md-2 d-flex justify-content-center">
+            <div className="p-4 d-block text-decoration-none">
+              <div className="content">
+                <p>
+                  The Empathize section of my portfolio is a comprehensive guide
+                  to understanding users and their experiences. By delving into
+                  these empathy activities, you will gain a deeper appreciation
+                  of the user-centered design process and how it leads to more
+                  effective and meaningful design solutions.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
--- src/app/caseStudyTwo/page.tsx
+++ src/app/caseStudyTwo/page.tsx
@@ -1,17 +1,79 @@
 "use client";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Link from "next/link";
 import "/src/app/styles/global.css";
 
-
 export default function caseStudyTwo() {
-    return (
-      <div>
-        <p>Case Study Two</p>
-        <p>Case Study Two</p>
-        {/* Add more content about yourself here */}
-      </div>
-    );
-  };
-  
+  return (
+    <div>
+      <section className="container text-center py-5">
+        <h2 className="mb-5 mt-5">CASE STUDY TWO</h2>
+        <p className="mb-5 mt-5">
+          PROMPT: Design an app and a responsive website for a public art museum
+          to advertise exhibitions and events, provide museum information to
+          patrons, and enable patrons to schedule visits.
+        </p>
+        
+        <div className="d-flex justify-content-center mt-5">
+          <div className="row justify-content-center mt-5">
+            <div className="col-md-2 d-flex justify-content-center">
+              <Link
+                href="/caseStudyTwo/empathize"
+                className="btn btn-light p-4 d-block shadow text-decoration-none"
+              >
+                <div className="img-fluid">
+                  <img src="/Empathize.png" />
+                </div>
+              </Link>
+            </div>
+
+            <div className="col-md-2 d-flex justify-content-center">
+              <Link
+                href="/caseStudyTwo/define"
+                className="btn btn-light p-4 d-block shadow text-decoration-none"
+              >
+                <div className="img-fluid">
+                  <img src="/lifecyleImages/define.png" />
+                </div>
+              </Link>
+            </div>
+
+            <div className="col-md-2 d-flex justify-content-center">
+              <Link
+                href="/caseStudyTwo/ideate"
+                className="btn btn-light p-4 d-block shadow text-decoration-none"
+              >
+                <div className="img-fluid">
+                  <img src="/lifecyleImages/ideate.png" />
+                </div>
+              </Link>
+            </div>
+
+            <div className="col-md-2 d-flex justify-content-center">
+              <Link
+                href="/caseStudyTwo/prototype"
+                className="btn btn-light p-4 d-block shadow text-decoration-none"
+              >
+                <div className="img-fluid">
+                  <img src="/lifecyleImages/prototype.png" />
+                </div>
+              </Link>
+            </div>
+
+            <div className="col-md-2 d-flex justify-content-center">
+              <Link
+                href="/caseStudyTwo/test"
+                className="btn btn-light p-4 d-block shadow text-decoration-none"
+              >
+                <div className="img-fluid">
+                  <img src="/lifecyleImages/test.png" />
+                </div>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
