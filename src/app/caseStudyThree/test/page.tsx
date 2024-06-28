9fef13bce62aa43703065eacb72be457aeb72bdf
Mariola Hullings <mariolahullings@gmail.com>
2024-06-28 13:00:36 -0400

    Added usability study plan

--- src/app/caseStudyThree/test/page.tsx
+++ src/app/caseStudyThree/test/page.tsx
@@ -52,7 +52,7 @@ export default function test() {
       </div>
       <div className="row justify-content-center mt-5 pb-5">
         <ImageAndTextCard
-          href="#user-interviews"
+          href="#plan-usabilityStudy"
           imgSrc="/moduleIcons/planUsabilityStudy.png"
           text="Plan a usability study:  Develop a comprehensive UX research plan by defining research goals, questions, KPIs, methodology, participants, and script to effectively assess user interactions and usability."
         />
@@ -71,39 +71,17 @@ export default function test() {
       </div>
       <div className="container pt-5">
       <div className="row justify-content-center">
-        {/* <ImageCard imgSrc="/empathizeCS3/researchGoals.png" />
-        <ImageCard imgSrc="/empathizeCS3/audience.png" />
         <ImageCard
-          id="user-interviews"
-          imgSrc="/empathizeCS3/interviewQuestions.png"
+          id="plan-usabilityStudy"
+          imgSrc="/CS3Test/userResearch.png"
         />
-        <ImageCard imgSrc="/empathizeCS3/Anna.png" />
-        <ImageCard imgSrc="/empathizeCS3/Carlos.png" />
-        <ImageCard imgSrc="/empathizeCS3/Sophie.png" />
-        <ImageCard imgSrc="/empathizeCS3/Ravi.png" />
-        <ImageCard imgSrc="/empathizeCS3/Jasmine.png" />
-        <ImageCard imgSrc="/empathizeCS3/Michael.png" />
-        <ImageCard id="empathy-map" imgSrc="/empathyMaps/AnnaEmpathyMap.png" />
-        <ImageCard imgSrc="/empathyMaps/CarlosEmpathyMap.png" />
-        <ImageCard imgSrc="/empathyMaps/SophieEmpathyMap.png" />
-        <ImageCard imgSrc="/empathyMaps/RaviEmpathyMap.png" />
-        <ImageCard imgSrc="/empathyMaps/JasmineEmpathyMap.png" />
-        <ImageCard imgSrc="/empathyMaps/MichaelEmpathyMap.png" />
-
-        <ImageCard id="user-persona" imgSrc="/CS3Personas/PersonaAnna2.png" />
-        <ImageCard imgSrc="/CS3Personas/PersonaMichael2.png" />
-        <ImageCard imgSrc="/CS3Personas/PersonaEmma2.png" />
-
-        <ImageCard id="user-stories" imgSrc="/CS3UserStory/UserStoryAnna.png" />
-        <ImageCard imgSrc="/CS3UserStory/UserStoryMichael.png" />
-        <ImageCard imgSrc="/CS3UserStory/UserStoryEmma.png" />
-
-        <ImageCard
-          id="User-Journey-Maps"
-          imgSrc="/CS3JourneyMap/JourneyMapAnna.png"
-        />
-        <ImageCard imgSrc="/CS3JourneyMap/JourneyMapMichael.png" />
-        <ImageCard imgSrc="/CS3JourneyMap/JourneyMapEmma.png" /> */}
+        <ImageCard imgSrc="/CS3Test/uxPlan1.png" />
+        <ImageCard imgSrc="/CS3Test/uxPlan2.png" />
+        <ImageCard imgSrc="/CS3Test/uxPlan3.png" />
+        <ImageCard imgSrc="/CS3Test/uxPlan4.png" />
+        <ImageCard imgSrc="/CS3Test/uxPlan5.png" />
+        <ImageCard imgSrc="/CS3Test/uxPlan6.png" />
+        <ImageCard imgSrc="/CS3Test/uxPlan7.png" />
       </div>
       </div>
       <button id="back-to-top" onClick={scrollToTop}>
